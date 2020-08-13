403004f10580a9ed28fc92b6e2a6426926261f03
PurpleDragonfly <10940221@qq.com>
2020-08-13 16:00:35 +0800

    update

--- src/webpack/getBabelConfig.js
+++ src/webpack/getBabelConfig.js
@@ -27,9 +27,7 @@ export default () => {
                 '@babel/plugin-transform-runtime',
                 {
                     helpers: false,
-                    polyfill: false,
-                    regenerator: true,
-                    moduleName: 'babel-runtime'
+                    regenerator: true
                 }
             ],
             [
