bf610a72decf0800dd63e1cadcff2462b0199aee
PurpleDragonfly <10940221@qq.com>
2020-10-21 10:37:41 +0800

    update

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## 更新日志
+
+## v1.0.15
+
+-   开发环境的 js 文件添加 hash 值, 去掉本解决本地调试的缓存问题
+-   file: webpack/webpack.dev.config.js
--- src/webpack/webpack.dev.config.js
+++ src/webpack/webpack.dev.config.js
@@ -1,11 +1,11 @@
-import path from 'path';
-import pluginsConfig from './plugins';
-import { merge } from 'webpack-merge';
-import webpackBaseConfig from './webpack.base.config';
-const cwdPath = process.cwd(); // 工程项目root path
+import path from 'path'
+import pluginsConfig from './plugins'
+import { merge } from 'webpack-merge'
+import webpackBaseConfig from './webpack.base.config'
+const cwdPath = process.cwd() // 工程项目root path
 
 const WebpackConfig = parentDirPath => (outputPath, filePath) => {
-    const output = outputPath ? outputPath : './build';
+    const output = outputPath ? outputPath : './build'
     return merge(webpackBaseConfig(parentDirPath), {
         mode: 'development',
         devtool: 'cheap-module-eval-source-map',
@@ -13,14 +13,14 @@ const WebpackConfig = parentDirPath => (outputPath, filePath) => {
         output: {
             path: path.resolve(cwdPath, output),
             filename: '[name].js',
-            chunkFilename: '[id].js',
+            // chunkFilename: '[id].js', //
             publicPath: '/'
         },
         resolve: {
             modules: [path.join(parentDirPath, 'node_modules')]
         },
         plugins: pluginsConfig({ htmlEntry: filePath, env: 'dev' })
-    });
-};
+    })
+}
 // export default WebpackConfig;
-module.exports = WebpackConfig;
+module.exports = WebpackConfig
