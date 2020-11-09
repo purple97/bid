c4f3f9cd4fc6d5c1393746093dba471219973713
PurpleDragonfly <10940221@qq.com>
2020-11-09 16:59:56 +0800

    babel插件的默认位置设置

--- src/webpack/getBabelConfig.js
+++ src/webpack/getBabelConfig.js
@@ -11,29 +11,29 @@ export default () => {
     let babelOptions = {
         babelrc: false,
         cwd: path.resolve(Utils.path.parentDir, 'node_modules'),
-        presets: [[require('@babel/preset-env'), { modules: 'commonjs' }], require('@babel/preset-react')],
+        presets: [['@babel/preset-env', { modules: 'commonjs' }], '@babel/preset-react'],
         plugins: [
             [
-                require('babel-plugin-react-scoped-css'),
+                'babel-plugin-react-scoped-css',
                 {
                     include: '.scoped.(le|c)ss$'
                 }
             ],
-            [require('@babel/plugin-proposal-decorators'), { legacy: true }],
-            require('@babel/plugin-proposal-optional-chaining'), // 可选链
-            require('@babel/plugin-proposal-nullish-coalescing-operator'), // 双问号
-            require('@babel/plugin-proposal-class-properties'),
-            require('@babel/plugin-proposal-object-rest-spread'),
-            require('babel-plugin-add-module-exports'),
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            '@babel/plugin-proposal-optional-chaining', // 可选链
+            '@babel/plugin-proposal-nullish-coalescing-operator', // 双问号
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-object-rest-spread',
+            'babel-plugin-add-module-exports',
             [
-                require('@babel/plugin-transform-runtime'),
+                '@babel/plugin-transform-runtime',
                 {
                     helpers: false,
                     regenerator: true
                 }
             ],
             [
-                require('babel-plugin-import'),
+                'babel-plugin-import',
                 {
                     libraryName: 'antd',
                     libraryDirectory: 'es',
