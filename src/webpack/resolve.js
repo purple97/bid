a869081ee8b831a8a842a44666a18c7fda3978e7
PurpleDragonfly <10940221@qq.com>
2022-09-19 11:29:11 +0800

    babel-loader等依赖找不到的问题

--- src/webpack/resolve.js
+++ src/webpack/resolve.js
@@ -15,11 +15,13 @@ const nodeAlias = {
     zlib: false,
     querystring: false
 }
+
 export default {
     modules: [
         path.join(Utils.path.cwdPath, 'src'),
         'node_modules', // 解决npm 8.x版本下部分包找不到依赖的问题
         path.join(Utils.path.cwdPath, 'node_modules'),
+        path.join(__dirname, '../..'),
         path.join(Utils.path.rootPath, 'node_modules')
     ],
     //require时候自动补全扩展名;
